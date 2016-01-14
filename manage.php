d5037841f96ad46f66497d7968f8d875dcb94ca8
Cabané <jorgecabane93@gmail.com>
2016-01-14 17:09:35 -0300

    modal

--- manage.php
+++ manage.php
@@ -101,6 +101,8 @@ $('table').find('tr').each(function(){
 </script>
 
 <script>
+var title = "Edit QR Code Print Page";
+var body = "cuerpo";
 function showModal(title, body) {
     var html = '<div class="modal fade" tabindex="-1" role="dialog">' +
     '<div class="modal-dialog">' +
