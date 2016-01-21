384694cb62bc0c32616c9671fec9727b60e8c275
Cabané <jorgecabane93@gmail.com>
2016-01-21 12:42:08 -0300

    modal

--- manage.php
+++ manage.php
@@ -23,6 +23,7 @@
  */
 require_once (dirname ( __FILE__ ) . '/../../config.php');
 require_once (dirname ( __FILE__ ) . '/locallib.php');
+//plug in to print elements inside modal
 $PAGE->requires->js ( new moodle_url ( $CFG->wwwroot . '/mod/attendance/jQuery.print.js' ) );
 $PAGE->requires->jquery ();
 $PAGE->requires->jquery_plugin ( 'ui' );
