8de9b6aea941670b02b07e6ac908852299f3d583
Cabané <jorgecabane93@gmail.com>
2016-01-21 15:02:36 -0300

    js for printing element

--- jQuery.print.js
+++ jQuery.print.js
@@ -1,237 +1,180 @@
-/* @license 
- * jQuery.print, version 1.3.3
- *  (c) Sathvik Ponangi, Doers' Guild
- * Licence: CC-By (http://creativecommons.org/licenses/by/3.0/)
- *--------------------------------------------------------------------------*/
-(function ($) {
-    "use strict";
-    // A nice closure for our definitions
-    function getjQueryObject(string) {
-        // Make string a vaild jQuery thing
-        var jqObj = $("");
-        try {
-            jqObj = $(string)
-                .clone();
-        } catch (e) {
-            jqObj = $("<span />")
-                .html(string);
+/// <reference path="http://code.jquery.com/jquery-1.4.1-vsdoc.js" />
+/*
+* Print Element Plugin 1.2
+*
+* Copyright (c) 2010 Erik Zaadi
+*
+* Inspired by PrintArea (http://plugins.jquery.com/project/PrintArea) and
+* http://stackoverflow.com/questions/472951/how-do-i-print-an-iframe-from-javascript-in-safari-chrome
+*
+*  Home Page : http://projects.erikzaadi/jQueryPlugins/jQuery.printElement 
+*  Issues (bug reporting) : http://github.com/erikzaadi/jQueryPlugins/issues/labels/printElement
+*  jQuery plugin page : http://plugins.jquery.com/project/printElement 
+*  
+*  Thanks to David B (http://github.com/ungenio) and icgJohn (http://www.blogger.com/profile/11881116857076484100)
+*  For their great contributions!
+* 
+* Dual licensed under the MIT and GPL licenses:
+*   http://www.opensource.org/licenses/mit-license.php
+*   http://www.gnu.org/licenses/gpl.html
+*   
+*   Note, Iframe Printing is not supported in Opera and Chrome 3.0, a popup window will be shown instead
+*/
+; (function (window, undefined) {
+    var document = window["document"];
+    var $ = window["jQuery"];
+    $.fn["printElement"] = function (options) {
+        var mainOptions = $.extend({}, $.fn["printElement"]["defaults"], options);
+        //iframe mode is not supported for opera and chrome 3.0 (it prints the entire page).
+        //http://www.google.com/support/forum/p/Webmasters/thread?tid=2cb0f08dce8821c3&hl=en
+        if (mainOptions["printMode"] == 'iframe') {
+            if ($.browser.opera || (/chrome/.test(navigator.userAgent.toLowerCase())))
+                mainOptions["printMode"] = 'popup';
         }
-        return jqObj;
-    }
+        //Remove previously printed iframe if exists
+        $("[id^='printElement_']").remove();
 
-    function printFrame(frameWindow, timeout) {
-        // Print the selected window/iframe
-        var def = $.Deferred();
-        try {
-            setTimeout(function () {
-                // Fix for IE : Allow it to render the iframe
-                frameWindow.focus();
-                try {
-                    // Fix for IE11 - printng the whole page instead of the iframe content
-                    if (!frameWindow.document.execCommand('print', false, null)) {
-                        // document.execCommand returns false if it failed -http://stackoverflow.com/a/21336448/937891
-                        frameWindow.print();
-                    }
-                } catch (e) {
-                    frameWindow.print();
-                }
-                frameWindow.close();
-                def.resolve();
-            }, timeout);
-        } catch (err) {
-            def.reject(err);
-        }
-        return def;
-    }
-
-    function printContentInNewWindow(content, timeout) {
-        // Open a new window and print selected content
-        var w = window.open();
-        w.document.write(content);
-        w.document.close();
-        return printFrame(w, timeout);
-    }
-
-    function isNode(o) {
-        /* http://stackoverflow.com/a/384380/937891 */
-        return !!(typeof Node === "object" ? o instanceof Node : o && typeof o === "object" && typeof o.nodeType === "number" && typeof o.nodeName === "string");
-    }
-    $.print = $.fn.print = function () {
-        // Print a given set of elements
-        var options, $this, self = this;
-        // console.log("Printing", this, arguments);
-        if (self instanceof $) {
-            // Get the node if it is a jQuery object
-            self = self.get(0);
-        }
-        if (isNode(self)) {
-            // If `this` is a HTML element, i.e. for
-            // $(selector).print()
-            $this = $(self);
-            if (arguments.length > 0) {
-                options = arguments[0];
-            }
-        } else {
-            if (arguments.length > 0) {
-                // $.print(selector,options)
-                $this = $(arguments[0]);
-                if (isNode($this[0])) {
-                    if (arguments.length > 1) {
-                        options = arguments[1];
-                    }
-                } else {
-                    // $.print(options)
-                    options = arguments[0];
-                    $this = $("html");
-                }
-            } else {
-                // $.print()
-                $this = $("html");
-            }
-        }
-        // Default options
-        var defaults = {
-            globalStyles: true,
-            mediaPrint: false,
-            stylesheet: null,
-            noPrintSelector: ".no-print",
-            iframe: true,
-            append: null,
-            prepend: null,
-            manuallyCopyFormValues: true,
-            deferred: $.Deferred(),
-            timeout: 250
-        };
-        // Merge with user-options
-        options = $.extend({}, defaults, (options || {}));
-        var $styles = $("");
-        if (options.globalStyles) {
-            // Apply the stlyes from the current sheet to the printed page
-            $styles = $("style, link, meta, title");
-        } else if (options.mediaPrint) {
-            // Apply the media-print stylesheet
-            $styles = $("link[media=print]");
-        }
-        if (options.stylesheet) {
-            // Add a custom stylesheet if given
-            $styles = $.merge($styles, $('<link rel="stylesheet" href="' + options.stylesheet + '">'));
-        }
-        // Create a copy of the element to print
-        var copy = $this.clone();
-        // Wrap it in a span to get the HTML markup string
-        copy = $("<span/>")
-            .append(copy);
-        // Remove unwanted elements
-        copy.find(options.noPrintSelector)
-            .remove();
-        // Add in the styles
-        copy.append($styles.clone());
-        // Appedned content
-        copy.append(getjQueryObject(options.append));
-        // Prepended content
-        copy.prepend(getjQueryObject(options.prepend));
-        if (options.manuallyCopyFormValues) {
-            // Manually copy form values into the HTML for printing user-modified input fields
-            // http://stackoverflow.com/a/26707753
-            copy.find("input")
-                .each(function () {
-                    var $field = $(this);
-                    if ($field.is("[type='radio']") || $field.is("[type='checkbox']")) {
-                        if ($field.prop("checked")) {
-                            $field.attr("checked", "checked");
-                        }
-                    } else {
-                        $field.attr("value", $field.val());
-                    }
-                });
-            copy.find("select").each(function () {
-                var $field = $(this);
-                $field.find(":selected").attr("selected", "selected");
-            });
-            copy.find("textarea").each(function () {
-                // Fix for https://github.com/DoersGuild/jQuery.print/issues/18#issuecomment-96451589
-                var $field = $(this);
-                $field.text($field.val());
-            });
-        }
-        // Get the HTML markup string
-        var content = copy.html();
-        // Notify with generated markup & cloned elements - useful for logging, etc
-        try {
-            options.deferred.notify('generated_markup', content, copy);
-        } catch (err) {
-            console.warn('Error notifying deferred', err);
-        }
-        // Destroy the copy
-        copy.remove();
-        if (options.iframe) {
-            // Use an iframe for printing
-            try {
-                var $iframe = $(options.iframe + "");
-                var iframeCount = $iframe.length;
-                if (iframeCount === 0) {
-                    // Create a new iFrame if none is given
-                    $iframe = $('<iframe height="0" width="0" border="0" wmode="Opaque"/>')
-                        .prependTo('body')
-                        .css({
-                            "position": "absolute",
-                            "top": -999,
-                            "left": -999
-                        });
-                }
-                var w, wdoc;
-                w = $iframe.get(0);
-                w = w.contentWindow || w.contentDocument || w;
-                wdoc = w.document || w.contentDocument || w;
-                wdoc.open();
-                wdoc.write(content);
-                wdoc.close();
-                printFrame(w, options.timeout)
-                    .done(function () {
-                        // Success
-                        setTimeout(function () {
-                            // Wait for IE
-                            if (iframeCount === 0) {
-                                // Destroy the iframe if created here
-                                $iframe.remove();
-                            }
-                        }, 100);
-                    })
-                    .fail(function (err) {
-                        // Use the pop-up method if iframe fails for some reason
-                        console.error("Failed to print from iframe", err);
-                        printContentInNewWindow(content, options.timeout);
-                    })
-                    .always(function () {
-                        try {
-                            options.deferred.resolve();
-                        } catch (err) {
-                            console.warn('Error notifying deferred', err);
-                        }
-                    });
-            } catch (e) {
-                // Use the pop-up method if iframe fails for some reason
-                console.error("Failed to print from iframe", e.stack, e.message);
-                printContentInNewWindow(content, options.timeout)
-                    .always(function () {
-                        try {
-                            options.deferred.resolve();
-                        } catch (err) {
-                            console.warn('Error notifying deferred', err);
-                        }
-                    });
-            }
-        } else {
-            // Use a new window for printing
-            printContentInNewWindow(content, options.timeout)
-                .always(function () {
-                    try {
-                        options.deferred.resolve();
-                    } catch (err) {
-                        console.warn('Error notifying deferred', err);
-                    }
-                });
-        }
-        return this;
+        return this.each(function () {
+            //Support Metadata Plug-in if available
+            var opts = $.meta ? $.extend({}, mainOptions, $(this).data()) : mainOptions;
+            _printElement($(this), opts);
+        });
     };
-})(jQuery);
+    $.fn["printElement"]["defaults"] = {
+        "printMode": 'iframe', //Usage : iframe / popup
+        "pageTitle": '', //Print Page Title
+        "overrideElementCSS": null,
+        /* Can be one of the following 3 options:
+        * 1 : boolean (pass true for stripping all css linked)
+        * 2 : array of $.fn.printElement.cssElement (s)
+        * 3 : array of strings with paths to alternate css files (optimized for print)
+        */
+        "printBodyOptions": {
+            "styleToAdd": 'padding:10px;margin:10px;', //style attributes to add to the body of print document
+            "classNameToAdd": '' //css class to add to the body of print document
+        },
+        "leaveOpen": false, // in case of popup, leave the print page open or not
+        "iframeElementOptions": {
+            "styleToAdd": 'border:none;position:absolute;width:0px;height:0px;bottom:0px;left:0px;', //style attributes to add to the iframe element
+            "classNameToAdd": '' //css class to add to the iframe element
+        }
+    };
+    $.fn["printElement"]["cssElement"] = {
+        "href": '',
+        "media": ''
+    };
+    function _printElement(element, opts) {
+        //Create markup to be printed
+        var html = _getMarkup(element, opts);
+
+        var popupOrIframe = null;
+        var documentToWriteTo = null;
+        if (opts["printMode"].toLowerCase() == 'popup') {
+            popupOrIframe = window.open('about:blank', 'printElementWindow', 'width=650,height=440,scrollbars=yes');
+            documentToWriteTo = popupOrIframe.document;
+        }
+        else {
+            //The random ID is to overcome a safari bug http://www.cjboco.com.sharedcopy.com/post.cfm/442dc92cd1c0ca10a5c35210b8166882.html
+            var printElementID = "printElement_" + (Math.round(Math.random() * 99999)).toString();
+            //Native creation of the element is faster..
+            var iframe = document.createElement('IFRAME');
+            $(iframe).attr({
+                style: opts["iframeElementOptions"]["styleToAdd"],
+                id: printElementID,
+                className: opts["iframeElementOptions"]["classNameToAdd"],
+                frameBorder: 0,
+                scrolling: 'no',
+                src: 'about:blank'
+            });
+            document.body.appendChild(iframe);
+            documentToWriteTo = (iframe.contentWindow || iframe.contentDocument);
+            if (documentToWriteTo.document)
+                documentToWriteTo = documentToWriteTo.document;
+            iframe = document.frames ? document.frames[printElementID] : document.getElementById(printElementID);
+            popupOrIframe = iframe.contentWindow || iframe;
+        }
+        focus();
+        documentToWriteTo.open();
+        documentToWriteTo.write(html);
+        documentToWriteTo.close();
+        _callPrint(popupOrIframe);
+    };
+
+    function _callPrint(element) {
+        if (element && element["printPage"])
+            element["printPage"]();
+        else
+            setTimeout(function () {
+                _callPrint(element);
+            }, 50);
+    }
+
+    function _getElementHTMLIncludingFormElements(element) {
+        var $element = $(element);
+        //Radiobuttons and checkboxes
+        $(":checked", $element).each(function () {
+            this.setAttribute('checked', 'checked');
+        });
+        //simple text inputs
+        $("input[type='text']", $element).each(function () {
+            this.setAttribute('value', $(this).val());
+        });
+        $("select", $element).each(function () {
+            var $select = $(this);
+            $("option", $select).each(function () {
+                if ($select.val() == $(this).val())
+                    this.setAttribute('selected', 'selected');
+            });
+        });
+        $("textarea", $element).each(function () {
+            //Thanks http://blog.ekini.net/2009/02/24/jquery-getting-the-latest-textvalue-inside-a-textarea/
+            var value = $(this).attr('value');
+            //fix for issue 7 (http://plugins.jquery.com/node/13503 and http://github.com/erikzaadi/jQueryPlugins/issues#issue/7)
+            if ($.browser.mozilla && this.firstChild)
+                this.firstChild.textContent = value;
+            else
+                this.innerHTML = value;
+        });
+        //http://dbj.org/dbj/?p=91
+        var elementHtml = $('<div></div>').append($element.clone()).html();
+        return elementHtml;
+    }
+
+    function _getBaseHref() {
+        var port = (window.location.port) ? ':' + window.location.port : '';
+        return window.location.protocol + '//' + window.location.hostname + port + window.location.pathname;
+    }
+
+    function _getMarkup(element, opts) {
+        var $element = $(element);
+        var elementHtml = _getElementHTMLIncludingFormElements(element);
+
+        var html = new Array();
+        html.push('<html><head><title>' + opts["pageTitle"] + '</title>');
+        if (opts["overrideElementCSS"]) {
+            if (opts["overrideElementCSS"].length > 0) {
+                for (var x = 0; x < opts["overrideElementCSS"].length; x++) {
+                    var current = opts["overrideElementCSS"][x];
+                    if (typeof (current) == 'string')
+                        html.push('<link type="text/css" rel="stylesheet" href="' + current + '" >');
+                    else
+                        html.push('<link type="text/css" rel="stylesheet" href="' + current["href"] + '" media="' + current["media"] + '" >');
+                }
+            }
+        }
+        else {
+            $("link", document).filter(function () {
+                return $(this).attr("rel").toLowerCase() == "stylesheet";
+            }).each(function () {
+                html.push('<link type="text/css" rel="stylesheet" href="' + $(this).attr("href") + '" media="' + $(this).attr('media') + '" >');
+            });
+        }
+        //Ensure that relative links work
+        html.push('<base href="' + _getBaseHref() + '" />');
+        html.push('</head><body style="' + opts["printBodyOptions"]["styleToAdd"] + '" class="' + opts["printBodyOptions"]["classNameToAdd"] + '">');
+        html.push('<div class="' + $element.attr('class') + '">' + elementHtml + '</div>');
+        html.push('<script type="text/javascript">function printPage(){focus();print();' + ((!$.browser.opera && !opts["leaveOpen"] && opts["printMode"].toLowerCase() == 'popup') ? 'close();' : '') + '}</script>');
+        html.push('</body></html>');
+
+        return html.join('');
+    };
